436ccc87fefa8cc8154c597d4dff75f5b7774a04
MogNate18 <chachanathan10@gmail.com>
2026-06-23 12:11:08 +0300

    I just changed Hello World to Hello Nathan to see if they switch was okay.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const App = () => {
-  return (
-    <div>
-        Hello World
-    </div>
-  );
-}
+  return <div>Hello Nathan</div>;
+};
 
 export default App;
